3042b4f9b53c1b1960e9f1ccd995105f9285e3c6
qanstar <1435385924@qq.com>
2025-03-02 23:29:29 +0800

    feat: 完善header布局

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import { useAtom } from 'jotai';
 import styles from './index.module.less';
 import { fileNameAtom } from '../../store/file';
-import { MinusOutlined, BorderOutlined, CloseOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { MinusOutlined, BorderOutlined, CloseOutlined, SettingOutlined, MenuOutlined } from '@ant-design/icons';
+import { Button, Divider } from 'antd';
 import useWindowControl from '../../core/frame/useWindowControl';
 
 const Header = () => {
@@ -11,8 +11,14 @@ const Header = () => {
 
 	return (
 		<div className={styles.headerWarp}>
-			<div className={styles.title}>{fileName}</div>
-			<div className={styles.tools}>
+			<div className={styles.left}>
+				<Button type="text" icon={<MenuOutlined />} className={styles.windowButton} />
+				<div className={styles.title}>{fileName}</div>
+			</div>
+
+			<div className={styles.right}>
+				<Button type="text" icon={<SettingOutlined />} className={styles.windowButton} />
+				<Divider type="vertical" className={styles.divider} />
 				<Button type="text" icon={<MinusOutlined />} onClick={minimize} className={styles.windowButton} />
 				<Button type="text" icon={<BorderOutlined />} onClick={maximize} className={styles.windowButton} />
 				<Button type="text" icon={<CloseOutlined />} onClick={close} className={styles.windowButton} />
