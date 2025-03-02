facae46fc31aa75177325d353aff36a34c6d0b3e
qanstar <1435385924@qq.com>
2025-03-02 19:14:18 +0800

    feat: 补充18n和jotai全局状态管理

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
+import { useAtom } from 'jotai';
 import styles from './index.module.less';
+import { fileNameAtom } from '../../store/file';
 
 const Header = () => {
-	return <div className={styles.headerWarp}>Header</div>;
+	const [fileName] = useAtom(fileNameAtom);
+	return (
+		<div className={styles.headerWarp}>
+			<div className={styles.title}>{fileName}</div>
+			<div className={styles.tools}></div>
+		</div>
+	);
 };
 
 export default Header;
---
+++ src/store/file.ts
@@ -0,0 +1,6 @@
+import { atom } from 'jotai';
+import i18n from '../i18n';
+
+const fileNameAtom = atom(i18n.t('header.defaultTitle'));
+
+export { fileNameAtom };
