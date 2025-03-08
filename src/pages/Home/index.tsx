3c11cd34c39e0ef860292de0b6f38236012fd2e1
qanstar <1435385924@qq.com>
2025-03-08 15:51:39 +0800

    feat: 优化样式

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import Content from '../../components/Contetnt';
 import Header from '../../components/Header';
+import styles from './index.module.less';
 
 const Home = () => {
 	return (
-		<div>
-			<header>
+		<div className={styles.home}>
+			<header className={styles.header}>
 				<Header />
 			</header>
-			<main>
+			<main className={styles.main}>
 				<Content />
 			</main>
 		</div>
